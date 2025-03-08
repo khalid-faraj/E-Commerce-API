e5f9ccf59748d99f05ef151b4aaaf78fe51a27d3
Khalid <khalidfarajdev@gmail.com>
2025-03-09 00:28:57 +0200

    Creating specification class

---
+++ Entities/Specifications/BaseSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Specifications
+{
+	internal class BaseSpecification<T> : ISpecification<T>
+	{
+        public BaseSpecification(Expression<Func<T, bool>> filter)
+		{
+            Filter = filter;
+        }
+        public Expression<Func<T, bool>> Filter { get; }
+
+		public List<Expression<Func<T, object>>> Includes { get; } =
+			new List<Expression<Func<T, object>>>();
+
+		protected void AddIncludes(Expression<Func<T, object>> IncludeExpression)
+		{
+			Includes.Add(IncludeExpression);
+		}
+	}
+}
