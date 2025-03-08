1d85165530aba3078ba09ebcc380a6cea467460c
Khalid <khalidfarajdev@gmail.com>
2025-03-08 22:23:39 +0200

    Creating a generic repository and its interface

---
+++ DataAccess/RepositoriesImplementation/GenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.RepositoriesInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.RepositoriesImplementation
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : class
+	{
+		public Task<T> GetByIdAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<T>> ListAllAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Entities/RepositoriesInterfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RepositoriesInterfaces
+{
+	public interface IGenericRepository<T> where T : class
+	{
+		Task<T> GetByIdAsync(int id);
+		Task<IReadOnlyList<T>> ListAllAsync();
+	}
+}
