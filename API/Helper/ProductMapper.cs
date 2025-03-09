982e5017a1060b29cef1e159cf5ab8cc1da0a161
Khalid <khalidfarajdev@gmail.com>
2025-03-09 23:56:33 +0200

    Configuring AutoMapper profiles

--- API/Helper/ProductMapper.cs
+++ API/Helper/ProductMapper.cs
@@ -13,7 +13,10 @@ namespace Entities.Helper
 	{
 		public ProductMapper() 
 		{
-			CreateMap<Product, ProductToReturnDTO>();
+			CreateMap<Product, ProductToReturnDTO>()
+				.ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+				.ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+
 		}
 	}
 }
