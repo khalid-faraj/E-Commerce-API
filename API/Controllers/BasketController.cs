2194b5fb40c39a61e995665885baeb035cc957d9
Khalid <khalidfarajdev@gmail.com>
2025-03-16 01:29:45 +0200

    Adding the basket controller

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Models;
+using Entities.RepositoriesInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BasketController : ControllerBase
+	{
+		private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+		[HttpGet]
+		public async Task<ActionResult<CustomerBasket>> GetBasketByIdAsync(string id)
+		{
+			var basket = await _basketRepository.GetBasketAsync(id);
+			//Handling null.
+			return Ok(basket ?? new CustomerBasket(id));
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<CustomerBasket>> UpdateBasketAsync(CustomerBasket customerBasket)
+		{
+			var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
+			return Ok(updatedBasket);
+		}
+
+		[HttpDelete]
+		public async Task DeleteBasketAsync(string id)
+		{
+			await _basketRepository.DeleteBasketAsync(id);
+		}
+    }
+}
