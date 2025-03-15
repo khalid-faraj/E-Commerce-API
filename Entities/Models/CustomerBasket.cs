492ffe0df799d30641628c9bf4b140316d034cd7
Khalid <khalidfarajdev@gmail.com>
2025-03-16 00:18:38 +0200

    Setting up the basket classes

---
+++ Entities/Models/CustomerBasket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class CustomerBasket
+    {
+        public CustomerBasket()
+        {
+            
+        }
+        public CustomerBasket(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
