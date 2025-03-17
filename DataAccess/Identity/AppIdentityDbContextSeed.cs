5f31041a7033d3083e9a39402c570b42100da91a
Khalid <khalidfarajdev@gmail.com>
2025-03-17 17:12:45 +0200

    Seeding identity data

---
+++ DataAccess/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Identity
+{
+	public class AppIdentityDbContextSeed
+	{
+		public static async Task SeedUserAsync(UserManager<AppUser> userManager)
+		{
+			if(!userManager.Users.Any())
+			{
+				var user = new AppUser
+				{
+					DisplayName = "Khalid",
+					Email = "khalid@gmail.com",
+					UserName = "khalid@gmail.com",
+					Address = new Address
+					{
+						FirstName = "Khalid",
+						LastName = "Faraj",
+						Street = "kfj",
+						City = "PF",
+						State = "EG",
+						ZipCode = "12345"
+					}
+				};
+				await userManager.CreateAsync(user, "Pass123");
+			}
+		}
+	}
+}
