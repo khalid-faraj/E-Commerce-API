f29d2595a48529687150465950736cde60466013
Khalid <khalidfarajdev@gmail.com>
2025-03-23 17:55:07 +0200

    Creating Order Aggregate

---
+++ Entities/Models/Order Aggregate/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Order_Aggregate
+{
+	public class Order
+	{
+        public Order() { }
+        public Order(string buyerEmail, Address shippedToAddress, 
+            DeliveryMethod deliveryMethod, IReadOnlyList<OrderItem> orderItems, 
+            decimal subTotal)
+        {
+            BuyerEmail = buyerEmail;
+            ShippedToAddress = shippedToAddress;
+            DeliveryMethod = deliveryMethod;
+            OrderItems = orderItems;
+            SubTotal = subTotal;
+        }
+        public int Id { get; set; }
+        public string BuyerEmail { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Address ShippedToAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal SubTotal { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public string PaymentIntentId { get; set; }
+
+        public decimal GetTotal()
+        {
+            return SubTotal+DeliveryMethod.Price;
+        }
+
+    }
+}
---
+++ Entities/Models/Order Aggregate/ProductItemOrdered.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Order_Aggregate
+{
+	public class ProductItemOrdered
+	{
+        public ProductItemOrdered()
+        {
+            
+        }
+        public ProductItemOrdered(int productItemId, string productName, string picUrl)
+        {
+            ProductItemId = productItemId;
+            ProductName = productName;
+            PicUrl = picUrl;
+        }
+
+        public int ProductItemId { get; set; }
+        public string ProductName { get; set; }
+        public string PicUrl { get; set; }
+
+    }
+}
